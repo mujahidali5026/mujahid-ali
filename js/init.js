893c5f4f5c16ae20fdb2bff1ac926ff2adcacc89
mujahidali5026 <mali7222741@gmail.com>
2024-04-17 01:37:54 +0500

    blogs section added

--- js/init.js
+++ js/init.js
@@ -16,7 +16,7 @@ jQuery(document).ready(function () {
 	iknow_tm_portfolio();
 	iknow_tm_portfolio_popup();
 	iknow_tm_service_popup();
-	iknow_tm_news_popup();
+	// iknow_tm_news_popup(); 
 	iknow_tm_cursor();
 	iknow_tm_imgtosvg();
 	iknow_tm_popup();
@@ -233,34 +233,34 @@ function iknow_tm_service_popup() {
 // ----------------  NEWS POPUP  -------------------
 // -------------------------------------------------
 
-function iknow_tm_news_popup() {
+// function iknow_tm_news_popup() {
 
-	"use strict";
+// 	"use strict";
 
-	var modalBox = jQuery('.iknow_tm_modalbox');
-	var button = jQuery('.iknow_tm_news .iknow_tm_full_link,.iknow_tm_news .news_list ul li .details .title a');
-	var closePopup = modalBox.find('.close');
+// 	var modalBox = jQuery('.iknow_tm_modalbox');
+// 	var button = jQuery('.iknow_tm_news .iknow_tm_full_link,.iknow_tm_news .news_list ul li .details .title a');
+// 	var closePopup = modalBox.find('.close');
 
-	button.on('click', function () {
-		var element = jQuery(this);
-		var parent = element.closest('.list_inner');
-		var content = parent.find('.hidden_content').html();
-		var image = parent.find('.image .main').data('img-url');
-		var category = parent.find('.details .category a').text();
-		var title = parent.find('.details .title a').text();
-		modalBox.addClass('opened');
-		modalBox.find('.description_wrap').html(content);
-		modalBox.find('.news_informations').prepend('<div class="image"><img src="img/thumbs/4-2.jpg" alt="" /><div class="main" data-img-url="' + image + '"></div></div>');
-		modalBox.find('.news_informations .image').after('<div class="details"><span>' + category + '</span><h3>' + title + '</h3><div>');
-		iknow_tm_data_images();
-		return false;
-	});
-	closePopup.on('click', function () {
-		modalBox.removeClass('opened');
-		modalBox.find('.description_wrap').html('');
-		return false;
-	});
-}
+// 	button.on('click', function () {
+// 		var element = jQuery(this);
+// 		var parent = element.closest('.list_inner');
+// 		var content = parent.find('.hidden_content').html();
+// 		var image = parent.find('.image .main').data('img-url');
+// 		var category = parent.find('.details .category a').text();
+// 		var title = parent.find('.details .title a').text();
+// 		modalBox.addClass('opened');
+// 		modalBox.find('.description_wrap').html(content);
+// 		modalBox.find('.news_informations').prepend('<div class="image"><img src="img/thumbs/4-2.jpg" alt="" /><div class="main" data-img-url="' + image + '"></div></div>');
+// 		modalBox.find('.news_informations .image').after('<div class="details"><span>' + category + '</span><h3>' + title + '</h3><div>');
+// 		iknow_tm_data_images();
+// 		return false;
+// 	});
+// 	closePopup.on('click', function () {
+// 		modalBox.removeClass('opened');
+// 		modalBox.find('.description_wrap').html('');
+// 		return false;
+// 	});
+// }
 
 // -----------------------------------------------------
 // ---------------   MENU WIDTH   ----------------------
@@ -828,7 +828,7 @@ function iknow_tm_extra_menu() {
 		progress_by_frenify(li);
 		iknow_tm_portfolio_popup();
 		iknow_tm_service_popup();
-		iknow_tm_news_popup();
+		// iknow_tm_news_popup();
 		iknow_tm_popup();
 		iknow_tm_portfolio();
 		iknow_tm_contact_form();
